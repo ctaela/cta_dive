05080e8e8b23ca277f2e7df9dee0a9e0817c1cbb
ctaela <56516121+ctaela@users.noreply.github.com>
2021-08-01 17:02:23 -0500

    Add files via upload

--- display_cta_match.py
+++ display_cta_match.py
@@ -12,28 +12,65 @@ from bokeh.models.tools import HoverTool
 from bokeh.transform import linear_cmap
 from bokeh.io import output_notebook, show
 
-"""This will be used to display data from CTA matches."""
+"""This will be used to display stats from game matches."""
 
 latest_games = pd.read_csv('https://raw.githubusercontent.com/ctaela/cta_dive/master/data/reduced_games_27467.csv')
 latest_stats = pd.read_csv('https://raw.githubusercontent.com/ctaela/cta_dive/master/data/reduced_stats_27467.csv')
 
 game_list = list(latest_stats['Game ID'].sort_values(ascending=False).unique())
 
-# def show_table(df, game):
-#     source = ColumnDataSource(df[df["Game ID"] == game].sort_values(by=['Outcome', 'Kills'], ascending=False))
-#     columns = [
-#             TableColumn(field="Player", title="Player"),
-#             TableColumn(field="Outcome", title="Outcome"),
-#         ]
-#     data_table = DataTable(source=source, columns=columns, width=400, height=280)
-#     show(data_table)
+keep_list = ['Player', 'Team','Game ID', 'Player', 'Outcome', 'Team', 'Kills', 'Deaths', '+/-',
+       'Flag Captures', 'Flag Attempts', 'Death Chart','Switch', 'Elected', 'Assassinations',
+       'Score', 'Time Played', 'Damage Dealt', 'Damage Taken',
+       'DR', 'DKR', 'DDF', 'TDD', 'Laser DD', 'Laser DT', 'Missile DD',
+       'Missile DT', 'Grenade DD', 'Grenade DT', 'Bouncy DD', 'Bouncy DT']
+main_tab = ['Player', 'Outcome', 'Team', 'Kills', 'Deaths', '+/-',
+       'Flag Captures', 'Flag Attempts', 'Switch', 'Elected', 'Assassinations',
+       'Score', 'Time Played']
+damage_tab = ['Player', 'Team', 'Damage Dealt', 'Damage Taken','DR', 'DKR', 'DDF', 'TDD']
+weapon_tab = ['Player', 'Team', 'Laser DD', 'Laser DT', 'Missile DD','Missile DT', 
+              'Grenade DD', 'Grenade DT', 'Bouncy DD', 'Bouncy DT']
 
 game_option = st.selectbox(
     'Select game to view stats.', game_list)
 
-"""\nGame Stats"""
+damaged_cols = []
+killed_cols = []
 
-st.dataframe(data=latest_stats[latest_stats["Game ID"] == game_option].dropna(axis=1))
-# show_table(latest_stats, game_option)
+for col in test_df[test_df["Game ID"] == game_option].dropna(axis=1).columns:
+    if col not in ['Player', 'Team','Game ID', 'Player', 'Outcome', 'Team', 'Kills', 'Deaths', '+/-',
+       'Flag Captures', 'Flag Attempts', 'Death Chart','Switch', 'Elected', 'Assassinations',
+       'Score', 'Time Played', 'Damage Dealt', 'Damage Taken',
+       'DR', 'DKR', 'DDF', 'TDD', 'Laser DD', 'Laser DT', 'Missile DD',
+       'Missile DT', 'Grenade DD', 'Grenade DT', 'Bouncy DD', 'Bouncy DT']:
+        if col[0] == "K":
+            killed_cols.append(col)
+        else:
+            damaged_cols.append(col)
+
+"""
+Overview
+"""
+st.dataframe(data=latest_stats[latest_stats[main_tab]["Game ID"] == game_option].dropna(axis=1))
+
+"""
+Damage
+"""
+st.dataframe(data=latest_stats[latest_stats[damage_tab]["Game ID"] == game_option].dropna(axis=1))
+
+"""
+Weapons
+"""
+st.dataframe(data=latest_stats[latest_stats[weapon_tab]["Game ID"] == game_option].dropna(axis=1))
+
+"""
+Matchups - Kills
+"""
+st.dataframe(data=latest_stats[latest_stats[killed_cols]["Game ID"] == game_option].dropna(axis=1))
+
+"""
+Matchups - Damage
+"""
+st.dataframe(data=latest_stats[latest_stats[damaged_cols]["Game ID"] == game_option].dropna(axis=1))
 
 
